d9022fe3b9340c6eb6b698cb56196c9051905ef6
Mubassir Hayat <mubassirhayat@gmail.com>
2020-09-21 12:01:18 +0500

    launcher icons and app icons changed

--- lib/src/persistence/api_provider.dart
+++ lib/src/persistence/api_provider.dart
@@ -13,14 +13,17 @@ class ApiProvider {
     }
     city = city.toLowerCase();
     print('Got city $city');
-    final _baseUrl = "http://www.weather.opendatapakistan.info" + "/air-pak/$city/${_locationData.latitude}/${_locationData.longitude}";
+    final _baseUrl = "https://thawing-reef-17391.herokuapp.com" +
+        "/air-pak/$city/${_locationData.latitude}/${_locationData.longitude}";
     // final _baseUrl = "http://localhost:3000" + "/air-pak/$city/${_locationData.latitude}/${_locationData.longitude}";
     print("baseUrl $_baseUrl");
-    final response = await client.get("$_baseUrl"); // Make the network call asynchronously to fetch the data.
+    final response = await client.get(
+        "$_baseUrl"); // Make the network call asynchronously to fetch the data.
     print(response.body.toString());
 
     if (response.statusCode == 200) {
-      return Report.fromJson(json.decode(response.body)); //Return decoded response
+      return Report.fromJson(
+          json.decode(response.body)); //Return decoded response
     } else {
       throw Exception('Failed to load Data');
     }
